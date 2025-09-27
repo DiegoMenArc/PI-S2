c627f988860f4cdb84ef7e30bf345de7459f6dda
Rapha0800 <raphaelleite0800@gmail.com>
2025-09-27 16:03:36 -0300

    layout do carrossel-marca
    Adicionando a estetica do carrocel

--- src/app/pages/landing-page/pages/carrossel-marca/carrossel-marca.component.ts
+++ src/app/pages/landing-page/pages/carrossel-marca/carrossel-marca.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CarrosselComponent } from '../../../../components/carrossel/carrossel.component';
 
 @Component({
   selector: 'app-carrossel-marca',
-  imports: [],
+  imports: [CarrosselMarcaComponent],
   templateUrl: './carrossel-marca.component.html',
   styleUrl: './carrossel-marca.component.css'
 })
