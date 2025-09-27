95adf7ce7306edd11e5ec8c5c372c9a997902ba4
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-09-27 15:09:30 -0300

    carrossel landing page
    adicionando página de carrossel na landing page

---
+++ src/app/pages/landing-page/pages/carrossel-marca/carrossel-marca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrossel-marca',
+  imports: [],
+  templateUrl: './carrossel-marca.component.html',
+  styleUrl: './carrossel-marca.component.css'
+})
+export class CarrosselMarcaComponent {
+
+}
