aba2e45bbdaf3f6f0e5990110a4852a791e52b59
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-09-29 23:50:42 -0300

    Adicionando o html e css do menu

--- src/app/components/principal/menu/menu.component.ts
+++ src/app/components/principal/menu/menu.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UsuarioService } from '../../../services/usuario.service';
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+_usuarioAtual = inject(UsuarioService);
 
+  elementos = [
+    {nome:'Entrar'},
+    {nome:'Cadastrar'},
+    {nome:'Minhas compras'},
+    {nome:'Lista de presente'},
+    {nome:'Minhas avaliações'},
+    {nome:'Ajuda'},
+    {nome:'Política de privacidade'},
+    {nome:'Termos e condições'}
+  ]
+
+  caminho(){
+    let rota="";
+
+    (this._usuarioAtual.usuario==="")?rota="/login":rota="/usuario/10/informacoes";
+
+    return rota;
+  }
 }
