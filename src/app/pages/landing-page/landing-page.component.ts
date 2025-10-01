32df70fe561692da9f7785c2c784495a6fc4e4ee
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-10-01 16:30:17 -0300

    Tentando fazer funcionar o card menu

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -7,11 +7,12 @@ import { CarrosselMarcaComponent } from "./pages/carrossel-marca/carrossel-marca
 import { CardPromocaoComponent } from './card-promocao/card-promocao.component';
 import { VistoRecenteComponent } from './visto-recente/visto-recente.component';
 import { BotaoMenuComponent } from '../../components/principal/menu/botao-menu/botao-menu.component';
-import { BotaoHeaderComponent } from "../../components/principal/botao-header/botao-header.component";
+import { BotaoHeaderComponent } from '../../components/principal/botao-header/botao-header.component';
+import { NgIf } from "../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HeaderComponent, FooterComponent, CarrosselComponent, CarrosselMarcaComponent, CardPromocaoComponent, VistoRecenteComponent, BotaoMenuComponent, BotaoHeaderComponent],
+  imports: [HeaderComponent, FooterComponent, CarrosselComponent, CarrosselMarcaComponent, CardPromocaoComponent, VistoRecenteComponent, BotaoMenuComponent, BotaoHeaderComponent, NgIf],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
