25800af2aaf75a8d06be09c7863358d95a041094
DIEGO MENDES ARCHANJO <diego.marchanjo@senacsp.edu.br>
2025-10-03 17:50:14 -0300

    ajuste

--- src/app/pages/adm/adm.component.ts
+++ src/app/pages/adm/adm.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { AdmHeaderComponent } from '../../components/ADM/adm-header/adm-header.component';
 import { NavComponent } from '../../components/ADM/nav/nav.component';
 import { ListaPrdComponent } from '../../components/ADM/lista-prd/lista-prd.component';
+import { BreadcumbsComponent } from "../../components/principal/breadcumbs/breadcumbs.component";
 
 @Component({
   selector: 'app-adm',
-  imports: [ AdmHeaderComponent, NavComponent, ListaPrdComponent],
+  imports: [AdmHeaderComponent, NavComponent, ListaPrdComponent, BreadcumbsComponent],
   templateUrl: './adm.component.html',
   styleUrl: './adm.component.css'
 })
 export class AdmComponent {
-
+  paginas:String[ ] = ["Produtos"];
 }
