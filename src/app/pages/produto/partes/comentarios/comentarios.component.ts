111d0e178e15e6f892e7edf9aa255ed5565920df
DIEGO MENDES ARCHANJO <diego.marchanjo@senacsp.edu.br>
2025-10-03 16:47:03 -0300

    ajustes

--- src/app/pages/produto/partes/comentarios/comentarios.component.ts
+++ src/app/pages/produto/partes/comentarios/comentarios.component.ts
@@ -41,34 +41,34 @@ export class ComentariosComponent {
       data: "Postado em 18 de setembro de 2025 as 14:15"
     },
     {
-      nota : ['','','','',''],
-      nome: "Adaylson Arcanjo",
-      comentario: "Bacana isso ai cara",
-      data: "Postado em 18 de setembro de 2025 as 14:15"
+      nota : [''],
+      nome: "Glauber Larinjo",
+      comentario: "Comprei pq achei que dava pra morar mesmo, me decepcionei",
+      data: "Postado em 15 de setembro de 2025 as 12:24"
     },
     {
       nota : ['','','','',''],
-      nome: "Adaylson Arcanjo",
-      comentario: "Bacana isso ai cara",
-      data: "Postado em 18 de setembro de 2025 as 14:15"
+      nome: "Camilo Sabugosa.",
+      comentario: "Bom.",
+      data: "Postado em uma data."
     },
     {
       nota : ['','','','',''],
-      nome: "Adaylson Arcanjo",
-      comentario: "Bacana isso ai cara",
-      data: "Postado em 18 de setembro de 2025 as 14:15"
+      nome: "Dalva",
+      comentario: "Custa só 90 reais, mas eu pagaria R$2050 de tão bom que é",
+      data: "Postado em 13 de janeiro de 2025 as 20:32"
     },
     {
       nota : ['','','','',''],
-      nome: "Adaylson Arcanjo",
-      comentario: "Bacana isso ai cara",
-      data: "Postado em 18 de setembro de 2025 as 14:15"
+      nome: "Higor 2000",
+      comentario: "Bagui é bom memo, as cria tudo se amarro aqui em casa",
+      data: "Postado em 16 de março as 23:99"
     },
     {
-      nota : ['','','','',''],
-      nome: "Adaylson Arcanjo",
-      comentario: "Bacana isso ai cara",
-      data: "Postado em 18 de setembro de 2025 as 14:15"
+      nota : ['','','','',],
+      nome: "Vanessa Lopes",
+      comentario: "Produto excelente",
+      data: "Postado em 21 de agosto de 2025 as 19:01"
     }
   ]
 }
