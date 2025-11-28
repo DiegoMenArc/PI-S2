c3e6cebbb9290d6303730440cd6102513374247f
Adaylson Arcanjo <adaylsonevaristo15@gmail.com>
2025-11-27 22:24:23 -0300

    correção carrosel

--- src/app/components/cards/card-promocao/card-promocao.component.ts
+++ src/app/components/cards/card-promocao/card-promocao.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-card-promocao',
   imports: [],
   templateUrl: './card-promocao.component.html',
-  styleUrl: './card-promocao.component.css'
+  styleUrls: [
+    './card-promocao.component.css',
+  ],
 })
 export class CardPromocaoComponent {
 
